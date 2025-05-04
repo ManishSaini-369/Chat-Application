50d3c446661deb0f61c2663cc8bfc7b9ce907241
ManishSaini-369 <manishsaini1340@gmail.com>
2025-05-04 21:04:09 +0530

    backend auth completed with profile upload.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
-import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/auth.route.js'
+
 import { connectDB } from './lib/db.js'
 
 dotenv.config()
@@ -9,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
